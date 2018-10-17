e03cefcd142bb716aa5596d771dbcfc4d925a7f5
blacknightfury <blacknightfury828@gmail.com>
2018-10-18 03:52:40 +0400

    redux implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from "./screens/Dashboard";
-
+import { Provider } from "react-redux";
 import "./App.scss";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Route exact path="/" component={Dashboard} />
-        </Router>
-      </div>
-    );
-  }
-}
+const App = ({ store }) => (
+  <Provider store={store}>
+    <Router>
+      <Route exact path="/" component={Dashboard} />
+    </Router>
+  </Provider>
+);
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
+import { configureStore } from "./redux/store";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore();
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

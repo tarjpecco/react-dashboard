26dc40fe6286fb760e88eb204013de4e9294f17c
smartdev <tarjpecco777@gmail.com>
2018-11-30 13:32:42 -0500

    Update agent name field

--- frontend/src/screens/GC/MyInsurance/index.js
+++ frontend/src/screens/GC/MyInsurance/index.js
@@ -210,7 +210,7 @@ class MyInsurance extends React.Component {
 							{policies.map((policy, index) => (
 								<tr className="text-center" key={index}>
 									<td>
-										<p className="text-info">{policy.agent_name || ''}</p>
+										<p className="text-info">{(policy.agent[0] && policy.agent[0].first_name + policy.agent[0].last_name) || ''}</p>
 									</td>
 									<td>{policy.type}</td>
 									<td>{policy.number}</td>

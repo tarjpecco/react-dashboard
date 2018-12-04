bbe359cfd17f40effaccc552d02823f586abe3f8
smartdev <tarjpecco777@gmail.com>
2018-12-03 22:18:05 -0500

    Show progress jobs

--- frontend/src/redux/ducks/jobs.js
+++ frontend/src/redux/ducks/jobs.js
@@ -17,6 +17,8 @@ const jobsDuck = createDuck('jobs-duck');
 export const actions = createActions(jobsDuck,
   'CREATE_JOB',
   ...actionNames('GET_JOBS'),
+  ...actionNames('GET_RFQ_JOBS'),
+  ...actionNames('GET_PROGRESS_JOBS'),
   ...actionNames('UPDATE_JOB')
 )
 
@@ -25,10 +27,18 @@ export const stateSelector = state => state.jobs;
 export const getJobsSelector = createSelector([stateSelector], state => {
   return state.get('jobs').toJS();
 });
+export const getRFQJobsSelector = createSelector([stateSelector], state => {
+  return state.get('rfqJobs').toJS();
+});
+export const getProgressJobsSelector = createSelector([stateSelector], state => {
+  return state.get('progressJobs').toJS();
+});
 
 // Reducer Intial State
 const initialState = fromJS({
   jobs: [],
+  rfqJobs: [],
+  progressJobs: [],
   loading: false,
   error: null,
   updateError: null,
@@ -39,6 +49,14 @@ const jobListReducer = jobsDuck.createReducer({
   [actions.GET_JOBS_SUCCESS]: (state, { payload }) =>
     state
       .update('jobs', () => List(payload.jobList))
+      .set('loading', false),
+  [actions.GET_RFQ_JOBS_SUCCESS]: (state, { payload }) =>
+    state
+      .update('rfqJobs', () => List(payload.jobList))
+      .set('loading', false),
+  [actions.GET_PROGRESS_JOBS_SUCCESS]: (state, { payload }) =>
+    state
+      .update('progressJobs', () => List(payload.jobList))
       .set('loading', false),
   [actions.GET_JOBS_ERROR]: (state, { payload }) =>
     state
@@ -66,11 +84,17 @@ function* listJobsSaga({ payload }) {
     if (!isUndefined(payload.id)) {
       const { results } = yield call(getJobsForProject, payload.id, payload.status);
       jobList = results;
+      yield put(actions.get_jobs_success({ jobList }));
     } else {
       const { results } = yield call(getJobsByStatus, payload.status);
       jobList = results;
+      if (payload.status === 'rfq') {
+        yield put(actions.get_rfq_jobs_success({ jobList }));
+      }
+      if (payload.status === 'progress') {
+        yield put(actions.get_progress_jobs_success({ jobList }));
+      }
     }
-    yield put(actions.get_jobs_success({ jobList }));
   } catch (err) {
     const errorMessage = 'Listing jobs Failed';
     yield put(actions.get_jobs_error({ error: errorMessage }));
@@ -89,9 +113,31 @@ function* createJobSaga({ payload }) {
   }
 }
 
+function* listRFQJobsSaga() {
+  try {
+    const { results } = yield call(getJobsByStatus, 'rfq');
+    yield put(actions.get_rfq_jobs_success({ jobList: results }));
+  } catch (err) {
+    const errorMessage = 'Listing jobs Failed';
+    yield put(actions.get_jobs_error({ error: errorMessage }));
+  }
+}
+
+function* listProgressJobsSaga() {
+  try {
+    const { results } = yield call(getJobsByStatus, 'in_progress');
+    yield put(actions.get_progress_jobs_success({ jobList: results }));
+  } catch (err) {
+    const errorMessage = 'Listing jobs Failed';
+    yield put(actions.get_jobs_error({ error: errorMessage }));
+  }
+}
+
 export function* jobsSaga() {
   yield all([
     yield takeEvery(actions.GET_JOBS, listJobsSaga),
+    yield takeEvery(actions.GET_RFQ_JOBS, listRFQJobsSaga),
+    yield takeEvery(actions.GET_PROGRESS_JOBS, listProgressJobsSaga),
     yield takeLatest(actions.CREATE_JOB, createJobSaga),
   ]);
 }
